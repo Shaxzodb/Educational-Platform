f409092253b36b0441e7bfbf79430f30f8bada2c
shaxzodb <shaxzodbmaster@gmail.com>
2023-04-09 23:32:58 +0500

    profile edit

--- Users/forms.py
+++ Users/forms.py
@@ -1,43 +1,45 @@
-# from .models import Profile
-# from django import forms
-# from crispy_bootstrap5.bootstrap5 import FloatingField, BS5Accordion
-# from crispy_forms.helper import FormHelper
-# from crispy_forms.layout import Layout
-# from crispy_forms.bootstrap import AccordionGroup
+from .models import Profile
+from django import forms
+from crispy_bootstrap5.bootstrap5 import FloatingField, BS5Accordion
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout
+from crispy_forms.bootstrap import AccordionGroup
 
 
-# class ProfileForm(forms.ModelForm):
+class ProfileForm(forms.ModelForm):
     
-#     helper = FormHelper()
-#     helper.layout = Layout(
-#         # self.helper.form_tag = False
-#         # self.helper.include_media = False 
-#         BS5Accordion(
-#             AccordionGroup('#1',  
-#                 FloatingField("last_name", autocomplete="last_name"),
-#                 FloatingField("first_name", autocomplete="first_name"),
-#                 'user_pic'
-#             ),
-#             AccordionGroup('#2',
-#                 'bio',
-#             ),
-#             AccordionGroup('#3',
-#                 FloatingField("location", autocomplete="location"),
-#                 FloatingField("website", autocomplete="website"),
-#                 FloatingField("phone", autocomplete="phone"),
-#                 FloatingField("birth_date", autocomplete="birth_date"),
+    helper = FormHelper()
+    helper.layout = Layout(
+        # self.helper.form_tag = False
+        # self.helper.include_media = False 
+        BS5Accordion(
+            AccordionGroup('#1',  
+                FloatingField("last_name", autocomplete="last_name"),
+                FloatingField("first_name", autocomplete="first_name"),
+                'user_pic'
+            ),
+            AccordionGroup('#2',
+                'bio',
+            ),
+            AccordionGroup('#3',
+                FloatingField("location", autocomplete="location"),
+                FloatingField("website", autocomplete="website"),
+                FloatingField("phone", autocomplete="phone"),
+                FloatingField("birth_date", autocomplete="birth_date"),
                 
-#             ),
-#             flush=True,
-#             # always_open=True
-#         )
-#     )
+            ),
+            flush=True,
+            # always_open=True
+        )
+    )
     
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         
         
-#     class Meta:
-#         model = Profile
-#         fields = ['bio','last_name','first_name','location','website','phone','user_pic',"birth_date"]
-#         widgets = {}
+    class Meta:
+        model = Profile
+        fields = ['bio','last_name','first_name','location','website','phone','user_pic',"birth_date"]
+        widgets = {
+            'birth_date':forms.TextInput(attrs={'type':'date'}),
+        }
--- Users/views.py
+++ Users/views.py
@@ -5,10 +5,14 @@ from django.urls import reverse
 from django.shortcuts import redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .forms import ProfileForm
 
 # Create your views here
-class ProfileView(DetailView):
+class ProfileView(UserPassesTestMixin,UpdateView):
     model = Profile
+    form_class = ProfileForm
     template_name = 'account/profile.html'
-    # def get_queryset(self):
-    #     return super().get_queryset().select_related('user')
+    def get_queryset(self):
+        return super().get_queryset().select_related('user')
+    def test_func(self):
+        return self.get_object().user.id == self.request.user.id
