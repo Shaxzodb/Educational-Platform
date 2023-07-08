34fa13c372da4ca2720f8532cc37cfe819207268
Shaxzod Bahtiyarov <shaxzodbmaster@gmail.com>
2023-07-09 03:36:43 +0500

    Update urls.py

--- Config/urls.py
+++ Config/urls.py
@@ -18,10 +18,13 @@ from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.urls import path, include
+from Users.views import LoginView, SignupView
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
     path("ckeditor5/", include('django_ckeditor_5.urls'), name="ck_editor_5_upload_file"),
+    path('accounts/login/', LoginView.as_view(), name="account_login"),
+    path('accounts/signup/', SignupView.as_view(), name="account_signup"),
     path('accounts/', include('allauth.urls')),
 ] + i18n_patterns(
     path('admin/', admin.site.urls),
@@ -32,7 +35,7 @@ urlpatterns = [
     path('', include('Articles.urls')),
     path('', include('Comments.urls')),
     # path('register/', include('django.contrib.auth.urls')),
-    
+
 ) + static(
     settings.MEDIA_URL, document_root = settings.MEDIA_ROOT
 ) + static(
